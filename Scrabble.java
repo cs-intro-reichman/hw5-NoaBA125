7cbe3636c1198867427e94de73ab5eec23e55302
NoaBA125 <noa.benami10@gmail.com>
2024-12-12 18:04:42 +0200

    5.4

--- Scrabble.java
+++ Scrabble.java
@@ -125,7 +125,7 @@ public class Scrabble {
 			else {
 				if (isWordInDictionary(input)) {
 				score += wordScore(input);
-				System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points.");
+				System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points.\n");
 				hand = MyString.remove(hand, input); 
 				}
 				else {
